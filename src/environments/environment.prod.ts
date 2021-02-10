d4c13f17a6e3e9d5ed794768cbd78ab916c221fc
JohanMolina-Dev <jsmolina49@gmail.com>
2021-02-09 20:49:51 -0500

    app-empleados con fireBase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC71la_1d8ahWCZc9Ns1WStvARzeYwJSZw",
+    authDomain: "empleados-94.firebaseapp.com",
+    projectId: "empleados-94",
+    storageBucket: "empleados-94.appspot.com",
+    messagingSenderId: "962064658917",
+    appId: "1:962064658917:web:ee58ab64527c6d4d957f0c"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyC71la_1d8ahWCZc9Ns1WStvARzeYwJSZw",
+    authDomain: "empleados-94.firebaseapp.com",
+    projectId: "empleados-94",
+    storageBucket: "empleados-94.appspot.com",
+    messagingSenderId: "962064658917",
+    appId: "1:962064658917:web:ee58ab64527c6d4d957f0c"
+  }
 };
 
 /*
